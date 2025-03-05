60c7d90adce316b9c0b50b34c6c3298024a93a7c
Diana <buurgr99@gmail.com>
2025-03-05 17:06:37 +0300

    Delete KR directory

--- KR/DianaApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DianaappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DianaApp'
--- KR/project01/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
-# Create your views here.
-
-# http://127.0.0.1:8000/
-
-def index(request):
-    return HttpResponse("Добро пожаловать!") 
